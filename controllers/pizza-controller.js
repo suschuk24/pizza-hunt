84cea74f7d07f10614a835672fd2bc36fa96abe9
Seth Uschuk <suschuk24@gmail.com>
2020-11-24 11:29:27 -0600

    added comment routes, integrated with front end

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,12 @@ const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err)
@@ -12,18 +18,23 @@ const pizzaController = {
     },
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
-            .then(dbPizzaData => {
-                if (!dbPizzaData) {
-                    res.status(404).json({ message: "No pizza found wtih this id!" })
-                    return
-                }
-                res.json(dbPizzaData)
-            })
-            .catch(err => {
-                console.log(err)
-                res.status(400).json(err)
-            })
-    },
+          .populate({
+            path: 'comments',
+            select: '-__v'
+          })
+          .select('-__v')
+          .then(dbPizzaData => {
+            if (!dbPizzaData) {
+              res.status(404).json({ message: 'No pizza found with this id!' });
+              return;
+            }
+            res.json(dbPizzaData);
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      },
     createPizza({ body }, res) {
         Pizza.create(body)
             .then(dbPizzaData => res.json(dbPizzaData))
